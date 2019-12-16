50edcb3ee2c95635dbdd4b64cf4f328f0023d5f7
Ritwik Saha <s.ritwik98@gmail.com>
2019-12-17 00:59:52 +0530

    handle exception

--- testing/gps/gps_basic.py
+++ testing/gps/gps_basic.py
@@ -2,11 +2,15 @@
 # RX -> Pin 8
 # TX -> Pin 10
 import serial
+from serial.serialutil import SerialException
 
 port = "/dev/serial0"
 
 ser = serial.Serial(port, baudrate=9600, timeout=1.0)
 
 while True:
-    line = ser.readline().decode('ascii', errors='replace')
-    print(line.strip())
+    try:
+        line = ser.readline().decode("ascii", errors="replace")
+        print(line.strip())
+    except SerialException:
+        pass
