79ea480119fd0ccb32d2661a0c41940b8847eb2f
Ritwik Saha <s.ritwik98@gmail.com>
2019-12-17 02:24:38 +0530

    more exceptions

--- testing/gps/gps_basic.py
+++ testing/gps/gps_basic.py
@@ -3,7 +3,7 @@
 # TX -> Pin 10
 import serial
 from serial.serialutil import SerialException
-from .gps_decoder import decodeGPGGA, NotGPGGAError
+from gps_decoder import decodeGPGGA, NotGPGGAError, FixNotAcquiredError
 
 port = "/dev/serial0"
 
@@ -16,6 +16,8 @@ while True:
             print(decodeGPGGA(line))
         except NotGPGGAError:
             pass
+        except FixNotAcquiredError:
+            print("GPS fix not acquired")
     except SerialException:
         print("Exception Raised!!")
         ser.close()
--- testing/gps/gps_decoder.py
+++ testing/gps/gps_decoder.py
@@ -5,13 +5,20 @@ class NotGPGGAError(Exception):
     pass
 
 
+class FixNotAcquiredError(Exception):
+    pass
+
+
 def decodeGPGGA(line):
     line = line.decode("ascii", errors="replace").strip()
     parts = line[1:].split(",")
     if parts[0] != "GPGGA":
         raise NotGPGGAError
-    raw_lat = float(parts[2])
-    raw_lon = float(parts[4])
+    try:
+        raw_lat = float(parts[2])
+        raw_lon = float(parts[4])
+    except ValueError:
+        raise FixNotAcquiredError
     lat = math.modf(raw_lat / 100.0)[1] + math.modf(raw_lat / 100.0)[0] * (10.0 / 6.0)
     lon = math.modf(raw_lon / 100.0)[1] + math.modf(raw_lon / 100.0)[0] * (10.0 / 6.0)
     alt = float(parts[9])
