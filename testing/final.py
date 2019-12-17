6cea837c21a51a2554d738108be9feb422f95a8c
Ritwik Saha <s.ritwik98@gmail.com>
2019-12-18 00:03:49 +0530

    print

--- testing/final.py
+++ testing/final.py
@@ -49,13 +49,13 @@ if __name__ == "__main__":
         sleep(0.9)
         d.set_val_ratio(d.thr, d.thr_stable_ratio)
 
-        d.set_val_ratio(d.ele, 0.05)
+        print(d.set_val_ratio(d.ele, 0.05))
         sleep(1.0)
         d.control("0")
-        d.set_val_ratio(d.ail, 0.05)
+        print(d.set_val_ratio(d.ail, 0.05))
         sleep(1.0)
         d.control("0")
-        d.set_val_ratio(d.ele, -0.05)
+        print(d.set_val_ratio(d.ele, -0.05))
         sleep(1.0)
         d.control("0")
 
