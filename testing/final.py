75b49489424ca63ba630ee6ff687363635a19337
Ritwik Saha <s.ritwik98@gmail.com>
2019-12-18 00:23:24 +0530

    tweak

--- testing/final.py
+++ testing/final.py
@@ -50,13 +50,13 @@ if __name__ == "__main__":
         print(d.set_val_ratio(d.thr, d.thr_stable_ratio))
 
         print(d.set_val_ratio(d.ele, 0.07))
-        sleep(1.0)
+        sleep(2.0)
         d.control("0")
         print(d.set_val_ratio(d.ail, 0.07))
-        sleep(1.0)
+        sleep(2.0)
         d.control("0")
         print(d.set_val_ratio(d.ele, -0.07))
-        sleep(1.0)
+        sleep(2.0)
         d.control("0")
 
         print(d.set_val_ratio(d.thr, 0.85 * d.thr_stable_ratio))
